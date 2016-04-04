e3984e67447c8a48a07a3fe2f004d9e9c0607f17
jpsiyu <jpsiyu@163.com>
2016-04-04 18:15:09 +0800

    instanciate prefab

---
+++ Assets/Scripts/CopyMapView.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CopyMapView : MonoBehaviour {
+
+
+    private GameObject iconPrefab;
+    private GameObject iconBody;
+    private UITexture iconUp;
+
+    void Awake() {
+        BindElement();
+        GameObject.Instantiate(iconPrefab);
+
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    private void BindElement() {
+        iconPrefab = Resources.LoadAssetAtPath("Assets/Prefabs/CopyMapIcon.prefab", typeof(GameObject)) as GameObject;
+        iconBody = iconPrefab.transform.Find("offset/IconBody").gameObject;
+        iconUp = iconBody.transform.Find("IconUp").GetComponent<UITexture>();
+    }
+}
---
+++ Assets/Scripts/ViewManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ViewManager : MonoBehaviour {
+
+    private GameObject copyMapPrefab;
+
+    void Awake() {
+
+    }
+
+	// Use this for initialization
+	void Start () {
+        copyMapPrefab = Resources.LoadAssetAtPath("Assets/Prefabs/CopyMap.prefab", typeof(GameObject)) as GameObject;
+        GameObject copyMapGO = GameObject.Instantiate(copyMapPrefab) as GameObject;
+        copyMapGO.AddComponent<CopyMapView>();
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
